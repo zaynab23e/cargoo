59acc7609103b070d4baa417ff5e562410a4eb6c
zaynab10o <zaynabm766@gmail.com>
2025-07-22 17:51:54 +0300

    Add import statement for DriverResource in DriverAuthController

--- app/Http/Controllers/Driver/DriverAuthController.php
+++ app/Http/Controllers/Driver/DriverAuthController.php
@@ -9,6 +9,7 @@ use App\Models\Driver;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Resources\DriverResource;
 
 class DriverAuthController extends Controller
 {

829b57b9112231d722bff06584da88e75c48b38e
zaynab10o <zaynabm766@gmail.com>
2025-07-23 13:16:17 +0300

    Refactor register method in DriverAuthController to enable driver registration functionality

--- app/Http/Controllers/Driver/DriverAuthController.php
+++ app/Http/Controllers/Driver/DriverAuthController.php
@@ -12,32 +12,35 @@ use Illuminate\Support\Facades\Hash;
 
 class DriverAuthController extends Controller
 {
-//     public function register(request $request)
-//     {
-//         $validatedData = $request->validated();
-
-//         $driver = Driver::create($validatedData);
-//         $token = $driver->createToken('api-token')->plainTextToken;
-
-//         return response()->json([
-//             'message' => __('messages.register_success'),
-//             'driver' => $driver,
-//             'token' => $token,
-//         ]);
-//     }
-
-
-
-    public function login(login $request)
+    public function register(request $request)
     {
-        $validatedData = $request->validate([
+        $validatedData = $request->validat([
         'name' => 'required|string|max:255',
         'last_name' => 'nullable|string|max:255',
         'email' => 'required|email|unique:drivers,email',
         'phone' => 'required|string|max:15|unique:drivers,phone',
         'password' => 'required|string|confirmed|min:8',
-        'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-    ]);
+        // 'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $driver = Driver::create($validatedData);
+        
+        if (Driver::where('email', $request->email)->exists()) {
+            return response()->json(['message' => 'البريد الإلكتروني موجود بالفعل'], 409);
+        }
+        $token = $driver->createToken('api-token')->plainTextToken;
+        return response()->json([
+            'message' => __('messages.register_success'),
+            'driver' => $driver,
+            'token' => $token,
+        ]);
+    }
+
+
+
+    public function login(login $request)
+    {
+        $validatedData = $request->validat();
 
         $driver = Driver::where('email', $validatedData['email'])->first();
 
